2be4d4f0879c8849e80a974fa2812bfa1910d0f5
Francky <kyfr.rakotonirina1@gmail.com>
2025-04-11 19:43:55 +0200

    fixed sidebar

--- resources/views/elements/sidebar.blade.php
+++ resources/views/elements/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="deznav">
 	<div class="deznav-scroll">
 		<ul class="metismenu" id="menu">
-			<li><a href="{{ url('widget-basic')}}">Dashoard</a></li>
+			<li><a href="{{ url('/')}}">Dashoard</a></li>
 			<li><a href="{{ url('event')}}">Event</a></li>
 			<!-- <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
 					<i class="flaticon-025-dashboard"></i>
